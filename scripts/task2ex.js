38f5e086054bf45e2b40010b6d1def57f0e987c5
Washu-Misaki <92064229+Washu-Misaki@users.noreply.github.com>
2022-01-15 15:52:09 -0700

    task complete

--- scripts/task2ex.js
+++ scripts/task2ex.js
@@ -3,41 +3,40 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-const myName = "Daniel Jones";
+const myName = 'Brother Thayne';
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
 document.querySelector('#name').textContent = myName;
 
 // Step 3: declare and instantiate a variable to hold the current year
-const currentYear = 2022;
+const currentYear = 2020;
 
 // Step 4: place the value of the current year variable into the HTML file
 document.querySelector('#year').textContent = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-const myPicture = "../images/Daniel-Jones.jpg";
-
+const myPicture = 'images/photo.jpg';
 
 // Step 6: copy your image into the "images" folder
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-document.querySelector().setAttribute('src', myPicture);
+document.querySelector('img').setAttribute('src', myPicture);
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-const favoriteFoods = ['Enchiladas', 'Sushi', 'Spaghetti'];
+const favoriteFoods = ['Fettucini', 'Steak', 'Chicken', 'Shrimp'];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
 document.querySelector('#food').textContent = favoriteFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-const moreFavoriteFoods = ['Chocolate Chip Mint Ice Cream'];
+const anotherFavoriteFood = 'Baked Potato';
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-favoriteFoods.push(moreFavoriteFoods);
+favoriteFoods.push(anotherFavoriteFood);
 
 // Step 5: repeat Step 2
 document.querySelector('#food').textContent = favoriteFoods;
@@ -53,3 +52,5 @@ favoriteFoods.pop();
 
 // Step 7: repeat Step 2
 document.querySelector('#food').textContent = favoriteFoods;
+
+
